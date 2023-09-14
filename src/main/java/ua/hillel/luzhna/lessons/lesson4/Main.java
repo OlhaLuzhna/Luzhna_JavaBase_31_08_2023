4f6b1ec2f0256c1827e034a275f2580869d5b50e
OLuzhna <Olha.Luzhna@medeanalytics.com>
2023-09-14 21:01:26 +0300

    lesson4

---
+++ src/main/java/ua/hillel/luzhna/lessons/lesson4/Main.java
@@ -0,0 +1,17 @@
+package ua.hillel.luzhna.lessons.lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println(10 % 3); //залишок
+
+ //        a += 10; // a=a+10
+ //       System.out.println(a);
+
+        a++; //a=a+1
+        System.out.println(a);
+
+    }
+}
